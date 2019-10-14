5a75ddb3219b06417ec285f58290cc1f9b74d86d
gandol <suro.gento4@gmail.com>
2019-10-14 14:07:28 +0700

    perulangan da percabangan

---
+++ percabangan.php
@@ -0,0 +1,22 @@
+<?php
+// $hari = 'kamis';
+// if($hari == 'senin'){
+//     echo "praktikum coy";
+// }else if( $hari == 'selasa'){
+//     echo "kuliah";
+// }else{
+//     echo "waktunya libur";
+// }
+
+$tanggal = 10;
+
+switch($tanggal){
+    case 1:
+        echo "tanggal muda";
+        break;
+    case 27:
+        echo "tanggal tua, waktunya hemat";
+        break;
+    default:
+        echo "nanana";
+}
---
+++ perulangan.php
@@ -0,0 +1,6 @@
+<?php
+echo "Waktu mundur\n";
+for($count=10; $count>=0; $count--){
+    echo $count."\n";
+    sleep(1);
+}
