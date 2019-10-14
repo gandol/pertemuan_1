2dc57dd88a10b4e9c4618e758c55837aa0b71055
gandol <suro.gento4@gmail.com>
2019-10-14 13:28:56 +0700

    upload pertama

---
+++ hitung.php
@@ -0,0 +1,7 @@
+<?php
+//hitung luas jari" 
+$jari=10;
+$pi=3.14;
+$hasil=$jari*$jari*$pi;
+echo $hasil;
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+$nama 	= "gika";
+$umur 	= 16;
+$jari_jari = 7.12;
+
+// echo "nama saya ".$nama." umur : ".$umur." tahun";
+echo $jari_jari;
+
+?>
